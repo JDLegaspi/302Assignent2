951d88e737865373a777789938e6ba6e116fe80a
Okara Harvey <okaraemma@me.com>
2017-06-02 16:46:02 +1000

    Recovered tests

--- src/asgn2Tests/CustomerFactoryTests.java
+++ src/asgn2Tests/CustomerFactoryTests.java
@@ -5,6 +5,10 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import asgn2Customers.Customer;
+import asgn2Customers.CustomerFactory;
+import asgn2Exceptions.CustomerException;
+
 
 /**
  * A class the that tests the asgn2Customers.CustomerFactory class.
@@ -14,4 +18,40 @@ import org.junit.Test;
  */
 public class CustomerFactoryTests {
 	//TODO
+	@Test (expected=CustomerException.class) // !!
+	 public  void TestNameException () throws CustomerException{	
+		Customer customer = CustomerFactory.getCustomer("PUC", "abcdefghijklmnopqurstuvwxyz", "0412731942", 0, 0);
+	}
+	
+	@Test (expected=CustomerException.class)// !! 
+	 public  void TestNameExceptionTwo () throws CustomerException{	
+		Customer customer = CustomerFactory.getCustomer("PUC", "      ", "0412731942", 0, 0);
+	}
+	
+	@Test (expected=CustomerException.class)
+	 public  void TestMobileExceptionOne () throws CustomerException{	
+		Customer customer = CustomerFactory.getCustomer("PUC", "name", "2412731942", 0, 0);
+		
+	}
+	@Test (expected=CustomerException.class)
+	 public  void TestMobileExceptionTwo () throws CustomerException{	
+		Customer customer = CustomerFactory.getCustomer("PUC", "name", "041273192", 0, 0);	
+	}
+	
+	@Test (expected=CustomerException.class)
+	 public  void TestLocationException() throws CustomerException{	
+		Customer customer = CustomerFactory.getCustomer("DNC", "name", "0412731942", 12, 13);
+	}
+	
+	@Test (expected=CustomerException.class)//!!
+	 public  void TestTypeException() throws CustomerException{	
+		Customer customer = CustomerFactory.getCustomer("AMC", "name", "0412731942", 5, 2);
+	}
+	
+	@Test (expected=CustomerException.class)//!!
+	 public  void TestTDeliveryAtStore() throws CustomerException{	
+		Customer customer = CustomerFactory.getCustomer("AMC", "name", "0412731942", 0, 0);
+	}
+	
+
 }
--- src/asgn2Tests/CustomerTests.java
+++ src/asgn2Tests/CustomerTests.java
@@ -1,5 +1,12 @@
 package asgn2Tests;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import asgn2Customers.Customer;
+import asgn2Customers.CustomerFactory;
+import asgn2Exceptions.CustomerException;
 
 /**
  * A class that tests the that tests the asgn2Customers.PickUpCustomer, asgn2Customers.DriverDeliveryCustomer,
@@ -12,4 +19,56 @@ package asgn2Tests;
  */
 public class CustomerTests {
 	// TO DO
+	//Check Get anme 
+	@Test 
+	 public  void TestName() throws CustomerException{	
+		Customer customer = CustomerFactory.getCustomer("PUC", "name", "0412731942", 0, 0);
+		assertEquals("name", customer.getName());
+	}
+	
+
+	//mobile 
+	@Test 
+	 public  void TestMobile () throws CustomerException{	
+		Customer customer = CustomerFactory.getCustomer("PUC", "name", "0412731942", 0, 0);
+		assertEquals("0412731942", customer.getMobileNumber());
+	}
+	
+	//location Testing 
+	
+	@Test
+	 public  void TestLocationX() throws CustomerException{	
+		Customer customer = CustomerFactory.getCustomer("PUC", "name", "0412731942", 0, 0);
+		assertEquals(0, customer.getLocationX());
+	}
+	@Test
+	 public  void TestLocationY() throws CustomerException{	
+		Customer customer = CustomerFactory.getCustomer("PUC", "name", "0412731942", 0, 0);
+		assertEquals(0, customer.getLocationY());
+	}
+	
+	@Test 
+	 public  void TestTypePickUp() throws CustomerException{	
+		Customer customer = CustomerFactory.getCustomer("PUC", "name", "0412731942", 0, 0);
+		assertEquals("Pick-up Customer", customer.getCustomerType());
+	}
+	@Test 
+	 public  void TestTypeDrone() throws CustomerException{	
+		Customer customer = CustomerFactory.getCustomer("DNC", "name", "0412731942", 5, 2);
+		assertEquals("Drove Delivery Customer", customer.getCustomerType());
+	}
+	
+	@Test 
+	 public  void TestTypeDrive() throws CustomerException{	
+		Customer customer = CustomerFactory.getCustomer("DVC", "name", "0412731942", 5, 2);
+		assertEquals("Deliver Delivery Customer", customer.getCustomerType());
+	}
+	
+	
+	
+	
+	
+	
+	
+
 }
